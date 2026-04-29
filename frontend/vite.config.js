c97d82d45917b2af23766e6735d0cc7d76e32d17
skd19 <102909978+skd19@users.noreply.github.com>
2026-04-29 16:55:55 +0600

    Add 'fast-manga.onrender.com' to allowedHosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-	allowedHosts: ['8763-103-158-132-232.ngrok-free.app', 'localhost:8000', '*.ngrok-free.app'],
+	allowedHosts: ['8763-103-158-132-232.ngrok-free.app', 'localhost:8000', '*.ngrok-free.app', 'fast-manga.onrender.com'],
     port: 5173,
     proxy: {
       '/api': {
